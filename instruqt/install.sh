39c5bea2134a28248f5423e31c44c8852d29fc57
Brad Schmitt <bradleypschmitt@gmail.com>
2023-03-24 15:58:10 -0500

    install updates

--- instruqt/install.sh
+++ instruqt/install.sh
@@ -6,12 +6,12 @@ read -r value
 export NEW_RELIC_ACCOUNT_ID=$value
 
 printf 'Enter your New Relic License Key: '
-read value
+read -r value
 
 export NEW_RELIC_LICENSE_KEY=$value
 
 printf 'Enter your New Relic User API Key: '
-read value
+read -r value
 
 export NEW_RELIC_USER_KEY=$value
 
@@ -30,7 +30,7 @@ kubectl create secret generic newrelic-license-key --from-literal=license-key=$N
 function ver { printf "%03d%03d" $(echo "$1" | tr '.' ' '); } && \
 K8S_VERSION=$(kubectl version --short 2>&1 | grep 'Server Version' | awk -F' v' '{ print $2; }' | awk -F. '{ print $1"."$2; }') && \
 if [[ $(ver $K8S_VERSION) -lt $(ver "1.25") ]]; then KSM_IMAGE_VERSION="v2.6.0"; else KSM_IMAGE_VERSION="v2.7.0"; fi && \
-helm repo add newrelic https://helm-charts.newrelic.com && helm repo update newrelic && \
+helm repo add newrelic https://helm-charts.newrelic.com && helm repo update && \
 helm upgrade --install newrelic-bundle newrelic/nri-bundle \
  --set global.customSecretName=newrelic-license-key \
  --set global.customSecretLicenseKey=license-key \
@@ -49,7 +49,7 @@ echo "--------------------------------------------------"
 echo ""
 
 ## Install the Prometheus Agent using Helm
-helm repo add newrelic-prometheus https://newrelic.github.io/newrelic-prometheus-configurator && helm repo update newrelic-prometheus
+helm repo add newrelic-prometheus https://newrelic.github.io/newrelic-prometheus-configurator && helm repo update
 helm upgrade --install prometheus-agent newrelic-prometheus/newrelic-prometheus-agent -f ../prom-agent/values-simple.yaml -n newrelic
 
 echo ""
